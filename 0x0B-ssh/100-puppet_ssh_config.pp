52af7aa4b3db23fe65ca178bb36dd28627b02319
Iano-theDev <sinceianmike@gmail.com>
2022-09-23 20:18:22 +0300

    fix pp script

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # configure ssh to prevent password authentication
 exec { 'echo':
   path    => 'usr/bin:/bin',
-  command => 'echo " IdentifyFile ~/.ssh/school\n PasswordAuthentification no" >> /etc/ssh/ssh_config',
+  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
   returns => [0,1],
 }
