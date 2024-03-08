8b2fff5828bf29e3ec3010afcede13d290d7bce0
Iano-theDev <sinceianmike@gmail.com>
2024-03-08 10:38:13 +0300

    Update puupet script, remove extra comments

--- 0x0B-ssh/x.pp
+++
@@ -1,9 +0,0 @@
-# puppet script to configure SSH client
-# config 1. use privatee key in ~/.ssh/school
-# config 2.refuse to authenticate using a password
-
-exec { 'echo':
-  path    => 'usr/bin:bin',
-  command => 'echo "    IdentityFile ~/.ssh/school\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
-  returns => [0,1],
-}
